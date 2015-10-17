6b465d385260550b397e0c7f074bcfe14f670da0
ssaraf <ssaraf@visa.com>
2015-10-16 22:30:59 -0700

    added parse

--- app/src/main/java/com/crowdsocial/activity/EventDetailActivity.java
+++ app/src/main/java/com/crowdsocial/activity/EventDetailActivity.java
@@ -1,4 +1,4 @@
-package com.crowdsocial;
+package com.crowdsocial.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.crowdsocial.R;
 import com.squareup.picasso.Picasso;
 
 public class EventDetailActivity extends AppCompatActivity {
