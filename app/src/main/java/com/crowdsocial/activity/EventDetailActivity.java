83f52a4e52b3c467c590884555e1dde3dfb115be
Saurabh <saraf.saurabh@gmail.com>
2015-10-17 17:33:50 -0700

    Added Login/Register dialog Added BaseActivity Added mentu item for profile/account

--- app/src/main/java/com/crowdsocial/activity/EventDetailActivity.java
+++ app/src/main/java/com/crowdsocial/activity/EventDetailActivity.java
@@ -1,8 +1,6 @@
 package com.crowdsocial.activity;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -10,15 +8,15 @@ import android.widget.TextView;
 import com.crowdsocial.R;
 import com.squareup.picasso.Picasso;
 
-public class EventDetailActivity extends AppCompatActivity {
+public class EventDetailActivity extends BaseActivity {
 
-    ImageView ivEvent;
-    TextView tvLocation;
-    TextView tvAmount;
-    TextView tvParticipateCount;
-    TextView tvCommittedAmount;
-    TextView tvDateTime;
-    TextView tvDescription;
+    private ImageView ivEvent;
+    private TextView tvLocation;
+    private TextView tvAmount;
+    private TextView tvParticipateCount;
+    private TextView tvCommittedAmount;
+    private TextView tvDateTime;
+    private TextView tvDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,26 +46,10 @@ public class EventDetailActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_event_detail, menu);
-
-
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 }
