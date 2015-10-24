87b96d2b2268760a54c10f5ab66d392af7998bf7
Saurabh <saraf.saurabh@gmail.com>
2015-10-24 16:17:25 -0700

    participating events are not loaded properly

--- app/src/main/java/com/crowdsocial/fragment/ParticipatingEventsListFragment.java
+++ app/src/main/java/com/crowdsocial/fragment/ParticipatingEventsListFragment.java
@@ -3,11 +3,11 @@ package com.crowdsocial.fragment;
 import android.os.Bundle;
 
 import com.crowdsocial.model.Event;
+import com.crowdsocial.model.Invitee;
 import com.crowdsocial.util.ParseErrorHandler;
 import com.crowdsocial.util.ParseUserUtil;
 import com.parse.FindCallback;
 import com.parse.ParseException;
-import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
 import java.util.List;
@@ -26,23 +26,35 @@ public class ParticipatingEventsListFragment extends EventListFragment {
         super.onResume();
         removeAllEvents();
         if(ParseUserUtil.isUserLoggedIn()) {
-            populateUserEvents();
+            populateParticipatingEvents();
         }
     }
 
-    private void populateUserEvents() {
-        // Find all events by the current user
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("Event");
-        query.whereEqualTo("user", ParseUserUtil.getLoggedInUser());
-        query.findInBackground(new FindCallback<ParseObject>() {
+    private void populateParticipatingEvents() {
+        ParseQuery<Invitee> query = ParseQuery.getQuery(Invitee.class);
+        query.whereEqualTo("accepted", true);
+        query.whereEqualTo("email", ParseUserUtil.getLoggedInUser().getEmail());
+        query.findInBackground(new FindCallback<Invitee>() {
             @Override
-            public void done(List<ParseObject> objects, ParseException e) {
-                if(e != null) {
-                    e.printStackTrace();
+            public void done(List<Invitee> objects, ParseException e) {
+                if (e != null) {
                     ParseErrorHandler.handleError(e);
                 } else {
-                    if (objects.size() > 0) {
-                        addAllEvents((List<Event>) (List<?>) objects);
+                    for (Invitee i : objects) {
+                        ParseQuery<Event> query = ParseQuery.getQuery(Event.class);
+                        query.whereEqualTo("invitees", i);
+                        query.findInBackground(new FindCallback<Event>() {
+                            @Override
+                            public void done(List<Event> objects, ParseException e) {
+                                if (e != null) {
+                                    ParseErrorHandler.handleError(e);
+                                } else {
+                                    if (objects.size() > 0) {
+                                        addAllEvents((List<Event>) (List<?>) objects);
+                                    }
+                                }
+                            }
+                        });
                     }
                 }
             }
