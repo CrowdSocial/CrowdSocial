46435c9c347fb448583fb6c6219c586d477c7809
Saurabh <saraf.saurabh@gmail.com>
2015-10-31 17:38:08 -0700

    fixed cardview UI issues

--- app/src/main/java/com/crowdsocial/fragment/EventListFragment.java
+++ app/src/main/java/com/crowdsocial/fragment/EventListFragment.java
@@ -1,6 +1,7 @@
 package com.crowdsocial.fragment;
 
 import android.content.Intent;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -53,6 +54,8 @@ public class EventListFragment extends Fragment {
             }
         });
 
+        rvEvents.addItemDecoration(new SpacesItemDecoration(20, 10));
+
         return view;
     }
 
@@ -64,5 +67,27 @@ public class EventListFragment extends Fragment {
     public void removeAllEvents() {
         events.clear();
         aEvents.notifyDataSetChanged();
+    }
+
+    public class SpacesItemDecoration extends RecyclerView.ItemDecoration {
+        private int verticalSpace;
+        private int horizontalSpace;
+
+        public SpacesItemDecoration(int verticalSpace, int horizontalSpace) {
+            this.verticalSpace = verticalSpace;
+            this.horizontalSpace = horizontalSpace;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view,
+                                   RecyclerView parent, RecyclerView.State state) {
+            outRect.left = horizontalSpace;
+            outRect.right = horizontalSpace;
+            outRect.bottom = verticalSpace;
+
+            // Add top margin only for the first item to avoid double space between items
+            if(parent.getChildLayoutPosition(view) == 0)
+                outRect.top = verticalSpace;
+        }
     }
 }
