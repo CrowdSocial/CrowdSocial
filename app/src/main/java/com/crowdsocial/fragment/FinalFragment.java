ff1b1e2cf9784cadabd4365ccc1932fd21ced245
Saurabh <saraf.saurabh@gmail.com>
2015-11-01 19:52:04 -0800

    resolved contact list issues

--- app/src/main/java/com/crowdsocial/fragment/FinalFragment.java
+++ app/src/main/java/com/crowdsocial/fragment/FinalFragment.java
@@ -3,6 +3,7 @@ package com.crowdsocial.fragment;
 import android.app.Activity;
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
@@ -10,12 +11,15 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v4.widget.SimpleCursorAdapter;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.CheckedTextView;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.crowdsocial.R;
 
@@ -29,6 +33,7 @@ public class FinalFragment extends Fragment {
 
     private ContactsAdapter contactsAdapter;
     public static final int CONTACT_LOADER_ID = 78;
+    private SparseBooleanArray checkedContacts;
 
     private LoaderManager.LoaderCallbacks<Cursor> contactsLoader =
         new LoaderManager.LoaderCallbacks<Cursor>() {
@@ -91,16 +96,26 @@ public class FinalFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_final, container, false);
+        final View v = inflater.inflate(R.layout.fragment_final, container, false);
 
-        ListView lvContacts = (ListView) v.findViewById(R.id.lvContacts);
+        final ListView lvContacts = (ListView) v.findViewById(R.id.lvContacts);
         lvContacts.setAdapter(contactsAdapter);
+        checkedContacts = new SparseBooleanArray(lvContacts.getCount());
 
         lvContacts.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+                CheckedTextView tvName = (CheckedTextView) view.findViewById(R.id.tvName);
+                tvName.toggle();
+                if(tvName.isChecked()) {
+                    checkedContacts.put(position, true);
+                } else {
+                    checkedContacts.put(position, false);
+                }
+
                 Cursor c = ((ContactsAdapter) parent.getAdapter()).getCursor();
+
                 c.moveToPosition(position);
                 c.getString(c.getColumnIndex(ContactsContract.Contacts.PHOTO_URI));
                 listener.onContactSelected(
@@ -133,17 +148,56 @@ public class FinalFragment extends Fragment {
     }
 
     public class ContactsAdapter extends SimpleCursorAdapter {
+
+        private class ViewHolder {
+            private CheckedTextView tvName;
+            private ImageView ivProfileImage;
+            private TextView tvEmail;
+        }
+
         public ContactsAdapter(Context context, int layout, Cursor c, String[] from,
                                    int[] to, int flags) {
             super(context, layout, c, from, to, flags);
         }
 
         @Override
-        public void bindView(View view, Context context, Cursor cursor) {
-            super.bindView(view, context, cursor);
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            final ViewHolder viewHolder;
 
-            ImageView ivProfileImage = (ImageView) view.findViewById(R.id.ivProfileImage);
-//            ivProfileImage.setImageResource(R.drawable.calendar);
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext())
+                        .inflate(R.layout.item_contact, parent, false);
+                viewHolder = new ViewHolder();
+                viewHolder.tvName = (CheckedTextView) convertView.findViewById(R.id.tvName);
+                viewHolder.tvEmail = (TextView) convertView.findViewById(R.id.tvEmail);
+                viewHolder.ivProfileImage = (ImageView) convertView.findViewById(R.id.ivProfileImage);
+
+                convertView.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
+            if(checkedContacts.get(position)) {
+                viewHolder.tvName.setChecked(true);
+            } else {
+                viewHolder.tvName.setChecked(false);
+            }
+
+            getCursor().moveToPosition(position);
+
+            viewHolder.tvName.setText(
+                    getCursor().getString(getCursor().getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)));
+            viewHolder.tvEmail.setText(
+                    getCursor().getString(
+                            getCursor().getColumnIndex(ContactsContract.CommonDataKinds.Email.ADDRESS)));
+            String imageUri = getCursor().getString(
+                    getCursor().getColumnIndex(ContactsContract.Contacts.PHOTO_URI));
+
+            if(imageUri != null) {
+                viewHolder.ivProfileImage.setImageURI(Uri.parse(imageUri));
+            } else {
+                viewHolder.ivProfileImage.setImageDrawable(getResources().getDrawable(R.drawable.profile));
+            }
+            return convertView;
         }
     }
 }
